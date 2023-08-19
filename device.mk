59fa9eacadd0d72216c9f5791fedf9094ca14a03
muhammadrafiasyddiq <muhammadrafigaming8@gmail.com>
2023-08-19 17:24:53 +0700

    ADD VIRTUAL A/B

--- device.mk
+++ device.mk
@@ -24,7 +24,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Virtual A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
-
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 # Call proprietary blob setup
 $(call inherit-product-if-exists, vendor/xiaomi/rock/rock-vendor.mk)
 
