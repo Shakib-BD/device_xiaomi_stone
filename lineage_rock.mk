c9709e395b1c164e9f9f0a6fc097141f08bd0623
muhammadrafiasyddiq <muhammadrafigaming8@gmail.com>
2023-08-21 13:16:54 +0700

    Repair init,switch to GKI boot,Merge overlay to config.xml

--- lineage_rock.mk
+++ lineage_rock.mk
@@ -1,18 +1,7 @@
 #
-# Copyright (C) 2019 Potato Open Sauce Project
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2023 LineageOS
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
@@ -22,27 +11,27 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile
 $(call inherit-product, device/xiaomi/rock/device.mk)
 
-# Inherit some common Pixel-Experience stuff.
+# Inherit some common Rising stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Google - GMS
+WITH_GMS := true
+TARGET_CORE_GMS := true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := rock
 PRODUCT_NAME := lineage_rock
-PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := POCO M5
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := 22071219CG
 PRODUCT_MANUFACTURER := Xiaomi
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+BUILD_FINGERPRINT :=POCO/stone_p_global/stone:13/TP1A.220624.014/V14.0.7.0.TLUMIXM:user/release-keys
+PRIVATE_BUILD_DESC="stone_p_global-user 13 TP1A.220624.014 V14.0.7.0.TLUMIXM release-keys"
 
-PRODUCT_CHARACTERISTICS := nosdcard
-
-BUILD_FINGERPRINT :=Xiaomi/missi/missi:13/TP1A.220624.014/V14.0.7.0.TLUMIXM:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="missi_phoneext4_global-user 13 TP1A.220624.014 V14.0.7.0.TLUMIXM release-keys"
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_FACE_UNLOCK_SUPPORTED := true
-RISING_MAINTAINER := "RAFI_X"
-TARGET_ENABLE_BLUR := true
-WITH_GMS := true
-TARGET_CORE_GMS := true
+    PRODUCT_NAME="stone_p_global" \
+    PRODUCT_DEVICE="stone"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+RISING_MAINTAINER := "Rafi_x"
