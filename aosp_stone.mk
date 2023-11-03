42dd729cd46eb95d0490775fe0dbc71fae96ec7f
Briant Audiera <briantadr24@gmail.com>
2023-11-03 20:57:18 +0700

    stone: remove fuck lines

--- aosp_stone.mk
+++ aosp_stone.mk
@@ -16,13 +16,14 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier. This must come after all inclusions
+WITH_GMS := true
 PRODUCT_DEVICE := stone
 PRODUCT_NAME := aosp_stone
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := 22071219CG
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT :=POCO/stone_p_global/stone:13/TP1A.220624.014/V14.0.8.0.TLUMIXM:user/release-keys
-PRIVATE_BUILD_DESC="stone_p_global-user 13 TP1A.220624.014 V14.0.8.0.TLUMIXM release-keys"
+BUILD_FINGERPRINT :=POCO/stone_p_id/stone:13/TP1A.220624.014/V14.0.5.0.TLUIDXM:user/release-keys
+PRIVATE_BUILD_DESC="stone_p_global-user 13 TP1A.220624.014 V14.0.5.0.TLUIDXM release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
