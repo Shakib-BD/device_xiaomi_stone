f90f6c42b61f0554029b219b0750e85e41cee005
Briant Audiera <briantadr24@gmail.com>
2023-11-07 15:06:11 +0700

    stone: add framework comp matrix

--- device.mk
+++ device.mk
@@ -119,8 +119,13 @@ PRODUCT_PACKAGES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0-service \
+    android.hardware.keymaster@4.1-service \
     android.hardware.keymaster@4.0.vendor \
-    android.system.keystore2
+    android.system.keystore2 \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # IMS
 PRODUCT_PACKAGES += \
