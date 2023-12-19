5ab41444eebeeadcd119be02f4c53c53e3010a2f
Shakib <its.shakib@proton.me>
2023-12-19 12:36:53 +0600

    stone: Bringup DroidX-UI-13!
    Signed-off-by: Shakib <its.shakib@proton.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2019 Potato Open Sauce Project
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 The droidxOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,9 +16,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_stone.mk
+    $(LOCAL_DIR)/droidx_stone.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_stone-user \
-    lineage_stone-userdebug \
-    lineage_stone-eng
+    droidx_stone-user \
+    droidx_stone-userdebug \
+    droidx_stone-eng
---
+++ droidx_stone.mk
@@ -0,0 +1,48 @@
+#
+# Copyright (C) 2023 droidxOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile
+$(call inherit-product, device/xiaomi/stone/device.mk)
+
+# Inherit some common AlphaDroid stuff
+$(call inherit-product, vendor/droidx/config/common.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Gapps/Vanilla Flag
+DROIDX_GAPPS := CoreGapps
+
+# Maintainer Flag
+ro.droidx.maintainer=Shakib
+
+# droidxOS
+DROIDX_BUILD_TYPE := UNOFFICIAL
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# disable/enable blur support, default is false
+TARGET_ENABLE_BLUR := false
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+## Quick tap feature
+TARGET_SUPPORTS_QUICK_TAP := true
+
+PRODUCT_DEVICE := stone
+PRODUCT_NAME := droidx_stone
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := 22071219CG
+PRODUCT_MANUFACTURER := Xiaomi
+
+BUILD_FINGERPRINT :=POCO/stone_p_id/stone:13/TP1A.220624.014/V14.0.5.0.TLUIDXM:user/release-keys
+PRIVATE_BUILD_DESC="stone_p_global-user 13 TP1A.220624.014 V14.0.5.0.TLUIDXM release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
