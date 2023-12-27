c37ce6c5e5fa029195b1ca86c9440a62b19819ef
Shakib <its.shakib@proton.me>
2023-12-27 06:41:52 +0600

    stone: bringup pixelOs-t
    Signed-off-by: Shakib <its.shakib@proton.me>

--- aosp_stone.mk
+++ aosp_stone.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2023 droidxOS
+# Copyright (C) 2023 LineageOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,20 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile
 $(call inherit-product, device/xiaomi/stone/device.mk)
 
-# Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
-
-# Bootanimation res
+# Inherit some common crDroid stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# ArrowOS Maintainer
-DEVICE_MAINTAINER := 𝕊𝕙𝕒𝕜𝕚𝕓 🦄
-
-# Pixel Charger
-TARGET_INCLUDE_PIXEL_CHARGER := true
-
+## Device identifier. This must come after all inclusions
+WITH_GMS := true
 PRODUCT_DEVICE := stone
-PRODUCT_NAME := arrow_stone
+PRODUCT_NAME := aosp_stone
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := 22071219CG
 PRODUCT_MANUFACTURER := Xiaomi
