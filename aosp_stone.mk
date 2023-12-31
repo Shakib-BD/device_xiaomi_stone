c1f546af641f618871b51f4d83a23d48ca4407cc
Shakib <its.shakib@proton.me>
2023-12-31 09:35:15 +0600

    stone: Bringup PixelOS-UDC
    Signed-off-by: Shakib <its.shakib@proton.me>

--- aosp_stone.mk
+++ aosp_stone.mk
@@ -11,22 +11,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile
 $(call inherit-product, device/xiaomi/stone/device.mk)
 
-# Inherit some common Rising stuff
-$(call inherit-product, vendor/rising/config/rising.mk)
+# Inherit some common PixelOS-UDC stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-
-# Google - GMS & Pixel Feature
-WITH_GMS := true
-TARGET_CORE_GMS := true
-TARGET_CORE_GMS_EXTRAS := false
-TARGET_PREBUILT_PIXEL_LAUNCHER := false
-TARGET_ENABLE_PIXEL_FEATURES := false
-RISING_PACKAGE_TYPE := "CORE"
-TARGET_ENABLE_BLUR := false
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := stone
-PRODUCT_NAME := rising_stone
+PRODUCT_NAME := aosp_stone
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := 22071219CG
 PRODUCT_MANUFACTURER := Xiaomi
@@ -35,5 +26,3 @@ BUILD_FINGERPRINT :=POCO/stone_p_id/stone:13/TP1A.220624.014/V14.0.5.0.TLUIDXM:u
 PRIVATE_BUILD_DESC="stone_p_global-user 13 TP1A.220624.014 V14.0.5.0.TLUIDXM release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-RISING_MAINTAINER := "BriantAudiera"
-RISING_CHIPSET := "MT6789"
