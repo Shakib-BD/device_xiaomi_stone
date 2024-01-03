48ff5a660c14670f624fee668223a0549885b347
شَکِیْب | Shakib <52566642+Shakib-BD@users.noreply.github.com>
2024-01-03 17:39:47 +0600

    stone:  overlay dir change for pixel udc

--- device.mk
+++ device.mk
@@ -158,7 +158,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-aosp \
     $(LOCAL_PATH)/overlay-gapps
 
 PRODUCT_PACKAGES += \
